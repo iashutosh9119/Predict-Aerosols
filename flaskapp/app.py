16052d4d84664b85f8351296d3bd46a5cd4e5359
Ashutosh Mishra <iashutosh9119@gmail.com>
2024-10-21 11:45:22 +0900

    added windy

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 # Path to your GCP service account key JSON file
-SERVICE_ACCOUNT_FILE = 'config/creds2.json'
+SERVICE_ACCOUNT_FILE = '/home/ubuntu/ssta/config/creds2.json'
 
 # Load your Windy API key from an environment variable
 WINDY_API_KEY = "DHnqHp6YzeueWA6uhkK3cxT8USF5QsuX" #os.environ.get('WINDY_API_KEY')
